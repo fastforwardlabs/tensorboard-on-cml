f58af46862056841c075a5628ba57f79535919bb
Andrew Reed <andrew.reed.r@gmail.com>
2021-07-06 15:18:06 -0400

    Create load_and_train.py

---
+++ load_and_train.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import datetime
+import matplotlib.pyplot as plt
+
+
+# Load the MNIST digits dataset from Keras
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Function to create simple Keras model using Sequential API
+def create_model():
+  return tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
+
+# Create and fit a model by specifying the `tf.keras.callbacks.Tensorboard` Callback to ensure
+# logs are stored to capture information about the training run
+model = create_model()
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+model.fit(x=x_train, 
+          y=y_train, 
+          epochs=20, 
+          validation_data=(x_test, y_test), 
+          callbacks=[tensorboard_callback])
+
